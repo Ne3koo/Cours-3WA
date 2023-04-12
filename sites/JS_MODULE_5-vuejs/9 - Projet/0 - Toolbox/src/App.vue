f88975b21daf6e705c42c1fab85fca6bd15cb383
Flament Julien <julien.flmt@gmail.com>
2023-04-12 14:45:19 +0200

    add src

---
+++ sites/JS_MODULE_5-vuejs/9 - Projet/0 - Toolbox/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">{{ message }}</div>
+</template>
+
+<style scoped>
+  .container {
+    color: blue;
+    font-size: 2rem;
+    font-weight: bold;
+  }
+</style>
+
+<script>
+  export default {
+    name: 'App',
+    data() {
+      return {
+        message: 'Hello Vue!',
+      }
+    },
+  }
+</script>
