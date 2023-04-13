e289d2ad99b93293d6840d744a735ac1d7fdf591
Flament Julien <julien.flmt@gmail.com>
2023-04-13 16:30:42 +0200

    Push cours 13/04/2023

--- sites/JS_MODULE_5-vuejs/9 - Projet/0 - Toolbox/src/App.vue
+++ sites/JS_MODULE_5-vuejs/9 - Projet/0 - Toolbox/src/App.vue
@@ -1,22 +1,46 @@
 <template>
-  <div class="container">{{ message }}</div>
+  <div class="container">
+    <Header title="Welcome !" />
+    <Main>
+      <p>{{ content }}</p>
+    </Main>
+    <Footer copyright="Nevezide 2022" />
+  </div>
 </template>
 
-<style scoped>
-  .container {
-    color: blue;
-    font-size: 2rem;
-    font-weight: bold;
+<style>
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
   }
 </style>
 
 <script>
+  import Header from './components/Header.vue';
+  import Main from './components/Main.vue';
+  import Footer from './components/Footer.vue';
+
   export default {
-    name: 'App',
     data() {
       return {
-        message: 'Hello Vue!',
-      }
+        tasks: [{
+          description: 'Faire le ménage'
+        },{
+          description: 'Apprendre Vue'
+        },{
+          description: 'Faire un projet'
+        }]
+      };
+    },
+    components: {
+      Header,
+      Main,
+      Footer,
     },
   }
 </script>
---
+++ sites/JS_MODULE_5-vuejs/9 - Projet/0 - Toolbox/src/components/Main.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="main">
+        <ul>
+            <button v-on:click="">Ajouter une tâche</button>
+            <li v-for="item of tasks" class="li_style">
+                {{  item.description }}
+            </li>
+            <div class="add_task">
+                <p>description</p>
+                <input type="text" id="task_add">
+                <button>Enregistrer</button>
+            </div>
+        </ul>
+    </div>
+</template>
+
+<style>
+    .main { 
+        padding: 2em 1em;
+        font-family: sans-serif;
+        font-size: 1.2rem;
+        box-shadow:inset 0px 0px 20px -15px #252525;
+    }
+    .li_style {
+        list-style: none;
+        padding: 0.5em;
+        border: 1px solid rgba(0,0,0,0.2);
+        border-radius: 0.3em;
+        margin: 0.5em;
+    }
+</style>
+
+<script>
+export default ({
+    name: 'Main',
+    methods: {
+
+    },
+    data () {
+        return {
+            tasks: [{
+                description: 'Faire le ménage'
+            },
+            {
+                description: 'Apprendre Vue'
+            },
+            {
+                description: 'Faire un projet'
+            }]
+        }
+    },
+});
+</script>
